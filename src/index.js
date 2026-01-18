b33edaee311ae2f37d9d244d4a4744cc67cc2d1e
Santi-Burgos <santiburgos38@gmail.com>
2026-01-18 11:49:52 -0300

    fix: mejorando performance t2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', () => {
+    loadSecondaryFonts();
+  });
+} else {
+  setTimeout(loadSecondaryFonts, 0);
+}
+
+function loadSecondaryFonts() {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://fonts.googleapis.com/css2?family=Geist:wght@500;800&display=swap';
+  document.head.appendChild(link);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
